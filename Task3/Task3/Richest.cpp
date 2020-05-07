75ec4e7449008800571b6d4b445c957538e5b551
Dimentr01 <dima.zaharcev@mail.ru>
2020-05-07 20:42:30 +0300

    best commit

--- Task3/Task3/Richest.cpp
+++ Task3/Task3/Richest.cpp
@@ -6,22 +6,28 @@ void Richest(robbers Robber[], int &lifes)
 {
 	int k = 0;
 	int proverka = 0;
-	int richest =-1;
+	int richest = -999999;
 	for (int i = 0; i < lifes; i++)
-		if ((Robber[i].wealth > richest) and (Robber[i].life == 1))
-		{
-			richest = Robber[i].wealth;
-			k = i;
-		}
-	for (int i = 0; i < lifes; i++)
-		if ((Robber[i].wealth == richest) and (Robber[i].life == 1))
-		{
-			PrintRobber(Robber[i]);
-			proverka++;
-		}
+		if  (Robber[i].life == 1)
+	      richest =Robber[i].wealth;
+	if (richest != -999999)
+	{
+		for (int i = 0; i < lifes; i++)
+			if ((Robber[i].wealth > richest) and (Robber[i].life == 1))
+			{
+				richest = Robber[i].wealth;
+				k = i;
+			}
+		for (int i = 0; i < lifes; i++)
+			if ((Robber[i].wealth == richest) and (Robber[i].life == 1))
+			{
+				PrintRobber(Robber[i]);
+				proverka++;
+			}
+	}
 			
 	if (proverka == 1)
 		std::cout << "Богач найден" << std::endl << std::endl;
 	else if (proverka == 0) std::cout << "Не найдено" << std::endl << std::endl;
-	else std::cout << "Богачи найден" << std::endl << std::endl;
+	else std::cout << "Богачи найдены" << std::endl << std::endl;
 }
