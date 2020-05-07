195a3661d6ed9615a4b2ebdfbac1b93a0871b3d2
Dimentr01 <dima.zaharcev@mail.ru>
2020-05-07 23:15:15 +0300

    add func

--- Task3/Task3/Richest.cpp
+++ Task3/Task3/Richest.cpp
@@ -2,32 +2,40 @@
 #include "Robber.h"
 #include "Helper.h"
 
-void Richest(robbers Robber[], int &lifes)
+void Richest(robbers Robber[], int& lifes)
 {
-	int k = 0;
-	int proverka = 0;
-	int richest = -999999;
+	int richest;
+	bool isAlive = false;
 	for (int i = 0; i < lifes; i++)
-		if  (Robber[i].life == 1)
-	      richest =Robber[i].wealth;
-	if (richest != -999999)
 	{
-		for (int i = 0; i < lifes; i++)
-			if ((Robber[i].wealth > richest) and (Robber[i].life == 1))
-			{
-				richest = Robber[i].wealth;
-				k = i;
-			}
-		for (int i = 0; i < lifes; i++)
-			if ((Robber[i].wealth == richest) and (Robber[i].life == 1))
-			{
-				PrintRobber(Robber[i]);
-				proverka++;
-			}
+		if (Robber[i].life == 1)
+		{
+			richest = Robber[i].wealth;
+			isAlive = true;
+			break;
+		}
 	}
-			
-	if (proverka == 1)
-		std::cout << "Богач найден" << std::endl << std::endl;
-	else if (proverka == 0) std::cout << "Не найдено" << std::endl << std::endl;
-	else std::cout << "Богачи найдены" << std::endl << std::endl;
+
+	if (!isAlive)
+	{
+		std::cout << "Нет живых" << std::endl << std::endl;
+		return;
+	}
+
+	for (int i = 0; i < lifes; i++)
+		if ((Robber[i].wealth > richest) and (Robber[i].life == 1))
+			richest = Robber[i].wealth;
+
+	int singleRich = 0;
+	for (int i = 0; i < lifes; i++)
+		if ((Robber[i].wealth == richest) and (Robber[i].life == 1))
+		{
+			PrintRobber(Robber[i]);
+			singleRich++;
+		}
+
+	if (singleRich == 1)
+		std::cout <<" Богач найден" << std::endl << std::endl;
+	else
+		std::cout << "Богачи найдены" << std::endl << std::endl;
 }
